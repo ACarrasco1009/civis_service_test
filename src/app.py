c19b5a292e4bb3f6cb1a9b311f78ac12fbe4dd69
Unknown <alecc1009@gmail.com>
2018-08-30 10:04:02 -0400

    e

--- src/app.py
+++ src/app.py
@@ -2,9 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/test")
+@app.route("/")
 def hello():
     return "Hello!"
 
+@app.route("/test")
+def test():
+    return "Test!"
+
 if __name__ == '__main__':
-    app.run(port=5000, host='0.0.0.0')
+    app.run(port=5000)
