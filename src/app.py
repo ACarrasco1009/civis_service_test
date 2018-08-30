2b9fe1445deead7e690c751efadaffdfcd062d00
Unknown <alecc1009@gmail.com>
2018-08-30 09:44:52 -0400

    c

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Python!"
+    return "Hello!"
 
 if __name__ == '__main__':
-    app.run(port=3838)
+    app.run(port=5000)
